915eb0c09ad5f7eff4ffe791082668b670ce17c5
Nadav michael <nadav203@gmail.com>
2019-10-06 19:46:30 +0000

    MessageQueue send receive

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -4,22 +4,40 @@
 
 /* Implementation of class "MessageQueue" */
 
-/*
+
 template <typename T>
 T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
+  
+  std::unique_lock<std::mutex> unique_lock(_mutex);
+  _condition.wait(unique_lock, [this] {return !queue_.empty();});
+  
+  T msg- std::move(_queue.back());
+  _queue.pop_back();
+  return msg;
+  
+}
+             
 }
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
+  
+      // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+  
+  std::lock_guard<std::mutex> unique_lock(_mutex);
+  _queue.push_back(std::move(msg));
+  _condition.notify_one();
+
+
+
 }
-*/
+
 
 /* Implementation of class "TrafficLight" */
 
